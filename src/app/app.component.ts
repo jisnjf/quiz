93b85fce489608d8a064d187426cba7475ecc1cc
jisnjf <sofiejosefsen18@gmail.com>
2020-04-23 11:19:41 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,18 @@ import { Component,  } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  // En string der bliver brugt til at vise det navn som er blevet tastet ind.
   name = '';
-  player= '';  
+  // En string som lige nu er tom, men som lytter på hvad brugeren taster i inputfeltet
+  input= '';  
 
   SaveName() {
-    this.player= this.player;
-    console.log(this.player)
+    //Navnet sættes til at være ligemed det som brugeren taster ind i inputfeltet.
+    this.name= this.input;
+    console.log(this.name)
+    //Sætter variablen "input" til at være tom igen.
+    //Den sættes tom fordi det ønskes, at det som står i inputfeltet bliver slettet.
+    this.input="";
   }
   constructor()
 {
